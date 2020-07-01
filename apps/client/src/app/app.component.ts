c7f04527d39ffef5e3ed40c49906ecd8d21d43ae
Oleg Odnoral <4uiiakabpa@gmail.com>
2020-07-01 13:28:07 +0300

    Generate client application

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nest-nx-grpc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
